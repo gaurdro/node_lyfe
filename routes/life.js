6637ce3bbc61e0fb4e757f9b3bd8193aeb8ee9b0
Ross Smith <gaurdro@gaurdro.net>
2015-09-03 20:12:08 -0400

    flesh out app endpoints

---
+++ routes/life.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* these are the endpoints for the life simulator */
+// get / get's current state
+// put / sets/updates the current board
+// delete / resets the current board
+// post /next iterate n steps
+// get /delta get changes from last /next call
+router.get('/', function(req, res, next) {
+  res.send('get /');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('put /');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('delete /');
+});
+
+router.post('/next', function(req, res, next) {
+  res.send('post /next');
+});
+
+router.get('/delta', function(req, res, next) {
+  res.send('get /delta');
+});
+
+module.exports = router;
